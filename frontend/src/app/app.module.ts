d13e62732e0f5d45ef2237d341a44da86f4555bd
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2022-05-24 22:58:51 +0430

    install ngx-simple-modal and ngx-toastr add newly added modules to app-module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,11 +9,19 @@ import { LoginComponent } from './components/login/login.component';
 import { SecHeadersInterceptor } from './services/sec-headers.interceptor';
 import { WordIndexComponent } from './components/word-index/word-index.component';
 import { HomeComponent } from './components/home/home.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { SimpleModalModule } from 'ngx-simple-modal';
+import { LoadingComponent } from './components/loading/loading.component';
+import { ErrorHandlerService } from './services/error-handler.service';
+import { ToastNoAnimationModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    HomeComponent,
+    PaginationComponent,
+    LoadingComponent,
     WordIndexComponent,
     HomeComponent
   ],
@@ -21,6 +29,18 @@ import { HomeComponent } from './components/home/home.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    SimpleModalModule.forRoot({ container: 'modal-container'}, {
+      closeOnEscape: true,
+      closeOnClickOutside: true,
+      wrapperDefaultClasses: 'modal',
+      wrapperClass: 'o-modal',
+      animationDuration: 300,
+      autoFocus: true,
+      bodyClass: 'modal-body',
+      draggable: false,
+      draggableClass: '',
+    }),
+    ToastNoAnimationModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
@@ -29,6 +49,7 @@ import { HomeComponent } from './components/home/home.component';
       useClass: SecHeadersInterceptor,
       multi: true,
     },
+    ErrorHandlerService,
   ],
   bootstrap: [AppComponent]
 })
